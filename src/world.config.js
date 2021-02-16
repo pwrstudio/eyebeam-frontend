7f9e5ef4c0134394784c9830bf80fa294d928295
Emma Levesque <emma.levesque.schaefer@gmail.com>
2021-02-16 17:53:59 -0500

    updating "to Eyebeam Website"  link

--- src/world.config.js
+++ src/world.config.js
@@ -1,6 +1,6 @@
 // GENERAL
 export const SITE_TITLE = "Eyebeam"
-export const LINK_OUT = "https://www.eyebeam.org"
+export const LINK_OUT = "https://ftr.eyebeam.org"
 export const LINK_OUT_TEXT = "to Eyebeam website"
 export const MESSAGING_ENABLED = false
 // SERVERS
